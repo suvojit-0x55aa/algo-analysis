b250ef53c95a7eba48b1fcb81769f2a8250bef4d
shin-migami <davsuvo@gmail.com>
2016-07-01 23:30:01 +0530

    Add Stack Class

--- Week-02-StackQueue/ArrayStack.java
+++ Week-02-StackQueue/ArrayStack.java
@@ -1,17 +1,39 @@
 // Author : Shin
 
 import java.lang.*
+import java.lang.Integer;
 import java.util.Scanner;
 
 class Stack
 {
-    Integer elements[]  = new Integer[1];
-    int n = 0;
+    private Integer elements[]  = new Integer[1];
+    private int n = 0;
 
-    Boolean
+    Boolean is_empty()
+    {
+        return n == 0;
+    }
+
+    private void resize(int size)
+    {
+        Integer temp[] = new Integer[size];
+        for (Integer i : elements)
+            temp[i] = elements[i];
+        elements = temp;
+    }
 
     void push(Integer x)
     {
-        i
+        if (n = elements.length)
+            resize(2 * elements.length);
+        elements[n++] = x;
+    }
+
+    Integer pop()
+    {
+        Integer temp = elements[n--];
+        if(n > 0 && n < 2 * elements.length / 6)
+            resize(elements.length/2);
+        return  temp;
     }
 }
