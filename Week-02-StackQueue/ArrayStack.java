2972466798eb762a3ec9d49b559271a5f691261b
shin-migami <davsuvo@gmail.com>
2016-07-01 23:16:58 +0530

    Add Dir for Stack Queue

---
+++ Week-02-StackQueue/ArrayStack.java
@@ -0,0 +1,17 @@
+// Author : Shin
+
+import java.lang.*
+import java.util.Scanner;
+
+class Stack
+{
+    Integer elements[]  = new Integer[1];
+    int n = 0;
+
+    Boolean
+
+    void push(Integer x)
+    {
+        i
+    }
+}
