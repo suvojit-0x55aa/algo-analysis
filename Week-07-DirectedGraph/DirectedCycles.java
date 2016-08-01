542641f9ad0fdd6deac1dcc467ff949d85bf6121
shin-migami <davsuvo@gmail.com>
2016-08-01 19:50:23 +0530

    Add Directed Cycles

---
+++ Week-07-DirectedGraph/DirectedCycles.java
@@ -0,0 +1,216 @@
+//Author : Shin
+
+import java.lang.*;
+import java.util.*;
+
+class Bags<Item> implements Iterable<Item>
+{
+    private Node<Item> first;
+    private int n;
+
+    private static class Node<Item>
+    {
+        private Item item;
+        private Node<Item> next;
+    }
+
+    public Bags()
+    {
+        first = null;
+        n = 0;
+    }
+
+    public boolean is_empty()
+    {
+        return first == null;
+    }
+
+    public int size()
+    {
+        return n;
+    }
+
+    public void add(Item item)
+    {
+        Node<Item> temp = new Node<Item>();
+        temp.item = item;
+        temp.next = first;
+        first = temp;
+    }
+
+    public Iterator<Item> iterator()
+    {
+        return new ListIterator<Item>(first);
+    }
+
+    private class ListIterator<Item> implements Iterator<Item>
+    {
+        private Node<Item> current;
+
+        public ListIterator(Node<Item> first)
+        {
+            current = first;
+        }
+
+        public boolean hasNext()  { return current != null; }
+        public void remove()  { throw new UnsupportedOperationException(); }
+
+        public Item next()
+        {
+            if (!hasNext())  throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+}
+
+class DiGraphs
+{
+    private final int V;
+    private int E;
+
+    Bags<Integer> adj[];
+
+    DiGraphs(int n)
+    {
+        this.V = n;
+        this.E = 0;
+        adj = (Bags<Integer>[])new Bags[V];
+        for (int i = 0; i < V; ++i)
+            adj[i] = new Bags<Integer>();
+    }
+
+    DiGraphs(DiGraphs G)
+    {
+        this(G.V());
+        this.E = G.E();
+        for (int i = 0; i < V; ++i)
+        {
+            Bags<Integer> stack = new Bags<Integer>();
+
+            for (Integer x : G.adj[i])
+                stack.add(x);
+
+            for (Integer v  : stack)
+                this.adj[i].add(v);
+        }
+    }
+
+    public int V()  { return V; }
+    public int E()  { return E; }
+
+    private void valid_vertex(int v)
+    {
+        if (v < 0 || v > (V-1))  throw new IndexOutOfBoundsException();
+    }
+
+    public void add_edge(int v, int w)
+    {
+        valid_vertex(v);
+        valid_vertex(w);
+        adj[v].add(w);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v)
+    {
+        valid_vertex(v);
+        return adj[v];
+    }
+
+    public int degree(int v)
+    {
+        valid_vertex(v);
+        return adj[v].size();
+    }
+
+    public String toString()
+    {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " Vertex " + E + " Edges\n");
+        for (int i = 0; i < V; ++i)
+        {
+            s.append(i + " : ");
+            for (Integer w : adj[i])
+                s.append(w + " ");
+            s.append("\n");
+        }
+        return s.toString();
+    }
+}
+
+class DirectedCycles
+{
+    private boolean marked[];
+    private boolean onStack[];
+    private int edgeTo[];
+    private Stack<Integer> cycle;
+
+    DirectedCycles(DiGraphs G)
+    {
+        marked  = new boolean[G.V()];
+        onStack = new boolean[G.V()];
+        edgeTo  = new int[G.V()];
+
+        for (int i = 0; i < G.V(); ++i)
+            if (!marked[i] && cycle == null)  dfs(G, i);
+    }
+
+    private void dfs(DiGraphs G, int v)
+    {
+        marked[v]  = true;
+        onStack[v] = true;
+
+        for (int w : G.adj(v))
+        {
+            if (cycle != null)  return;
+
+            else if (!marked[w])
+            {
+                edgeTo[w] = v;
+                dfs(G, w);
+            }
+
+            else if (onStack[w])
+            {
+                cycle = new Stack<Integer>();
+                for (int x = v; x != w; x = edgeTo[x])
+                    cycle.push(x);
+                cycle.push(w);
+                cycle.push(v);
+            }
+        }
+        onStack[v] = false;
+    }
+
+    public boolean has_cycle() { return cycle != null; }
+
+    public Iterable<Integer> cycle() { return cycle; }
+}
+
+class RunApp
+{
+    public static void main(String args[])
+    {
+        Scanner readIn = new Scanner(System.in);
+
+        //Take Input
+        int vert = readIn.nextInt();
+        int edge = readIn.nextInt();
+        DiGraphs G = new DiGraphs(vert);
+        for (int i = 0; i < edge; ++i)
+        {
+            int v = readIn.nextInt();
+            int w = readIn.nextInt();
+            G.add_edge(v,w);
+        }
+
+        //Print if there exist a cycle
+        DirectedCycles DAG = new DirectedCycles(G);
+        if (DAG.has_cycle())
+            for (int v : DAG.cycle())
+                System.out.print(v + " ");
+        else  System.out.print("Cycles Don't Exist");
+    }
+}
